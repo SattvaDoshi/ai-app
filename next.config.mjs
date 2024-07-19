9022cae1141fef5589469c88c3d0b41ea782fdc9
SattvaDoshi <sattva103@gmail.com>
2024-07-19 21:18:40 +0530

    Deployment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false,
+          path: false,
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+          // Add this line for `node:async_hooks`:
+          async_hooks: require.resolve('node-libs-browser/mock/async_hooks'),
+        };
+      }
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
